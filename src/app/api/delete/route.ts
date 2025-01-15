2ea7659c129225977ad5da3100cb2e659ad78c02
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2025-01-15 10:13:57 -0300

    feat: I added a product removal route to the favorites list

---
+++ src/app/api/delete/route.ts
@@ -0,0 +1,33 @@
+import { prismaClient } from "@/lib/prisma";
+
+export async function DELETE(request: Request) {
+  const body = await request.json();
+  const { productId, wishListId } = body;
+
+  if (!productId || !wishListId) {
+    return Response.json(
+      { error: "ID do produto ou da wishlist não fornecido" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    await prismaClient.wishListProducts.deleteMany({
+      where: {
+        productId,
+        wishListId,
+      },
+    });
+
+    return Response.json(
+      { message: "Produto removido com sucesso da wishlist!" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Erro ao excluir produto da wishlist:", error);
+    return Response.json(
+      { error: "Erro ao excluir produto da wishlist" },
+      { status: 500 }
+    );
+  }
+}
